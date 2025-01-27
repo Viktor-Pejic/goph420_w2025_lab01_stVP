2aabf38b860ebd16538b4e11966d1bcc226d1465
Viktor Pejic <viktor.pejic@ucalgary.ca>
2025-01-26 21:54:12 -0700

    added raw data plot

---
+++ examples/raw_plot.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+s_wave = np.loadtxt("C:/Users/Viktor/GOPH_420/repos/goph420_w2025_lab01_stVP/data/s_wave_data.txt", dtype='float')
+
+x = s_wave[:, 0]
+f = s_wave[:, 1]
+
+plt.plot(x,f)
+plt.title('Time vs Velocity Plot')
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (mm/s)')
+plt.grid(True)
+plt.savefig("C:/Users/Viktor/GOPH_420/repos/goph420_w2025_lab01_stVP/figures/Raw_data.png")
+
+
